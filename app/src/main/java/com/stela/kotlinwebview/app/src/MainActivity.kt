b9a1dc03a924a4cbb911d3e6e20df6a8ed0d8ba8
Igor <gigor497@gmail.com>
2026-04-06 13:01:20 -0300

    Implemented WebView in the app

--- app/src/main/java/com/stela/kotlinwebview/app/src/MainActivity.kt
+++ app/src/main/java/com/stela/kotlinwebview/app/src/MainActivity.kt
@@ -1,10 +1,14 @@
-package com.stela.kotlinwebview
+package com.stela.kotlinwebview.app.src
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.stela.kotlinwebview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val webView = findViewById<WebView>(R.id.web)
+
+        webView.loadUrl("http://192.168.1.180/browser")
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+    }
+
+    fun initInsets() {
+        val fabNewScan = findViewById<FloatingActionButton>(R.id.fabNewScan)
     }
 }

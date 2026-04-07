fc28145158e09aabd941cba41ada422a533025d7
Igor <gigor497@gmail.com>
2026-04-07 15:38:52 -0300

    Created the Reader Screen for reading tags to associate to patients.

---
+++ app/src/main/java/com/stela/kotlinwebview/app/src/ScanScreen.kt
@@ -0,0 +1,24 @@
+package com.stela.kotlinwebview.app.src
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.stela.kotlinwebview.R
+
+class ScanScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_scan_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+
+}
